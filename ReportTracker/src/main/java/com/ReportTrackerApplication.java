149545887cb4502aff2959d50e8bb17d83d74639
Samuele Lettieri <samuele2073@gmail.com>
2024-06-27 17:44:44 +0200

    nuovo progetto

---
+++ ReportTracker/src/main/java/com/ReportTrackerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportTrackerApplication.class, args);
+	}
+
+}
