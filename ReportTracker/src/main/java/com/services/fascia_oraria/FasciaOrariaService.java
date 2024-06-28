d70be7fe76947a61256a958fef08419bedc17a33
riccardo98b <riccardobelloni98@gmail.com>
2024-06-28 17:44:53 +0200

    FasciaOrariaService e FasciaOrariaServiceImpl
    creato package fascia oraria dentro package services in cui ci sono FasciaOrariaService e FasciaOrariaServiceImpl

---
+++ ReportTracker/src/main/java/com/services/fascia_oraria/FasciaOrariaService.java
@@ -0,0 +1,5 @@
+package com.services.fascia_oraria;
+
+public interface FasciaOrariaService {
+
+}
---
+++ ReportTracker/src/main/java/com/services/fascia_oraria/FasciaOrariaServiceImpl.java
@@ -0,0 +1,8 @@
+package com.services.fascia_oraria;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FasciaOrariaServiceImpl implements FasciaOrariaService{
+
+}
