2b1e33723c147ee38fb28c1e3327c68a37d663ff
criCani <christiancanicoba159@gmail.com>
2024-06-28 14:22:35 +0200

    Create tipologiaCrimini table
    table created

---
+++ ReportTracker/src/main/java/com/model/TipologiaCrimine.java
@@ -0,0 +1,37 @@
+package com.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tipologieCrimini")
+public class TipologiaCrimine {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
