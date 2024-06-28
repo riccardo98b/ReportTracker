1c7412350902cc8d3c53e9b9593c8ceeed7860ac
Pako9129 <109146197+Pako9129@users.noreply.github.com>
2024-06-28 14:19:23 +0200

    FasciaOraria
    Creazione tabella

---
+++ ReportTracker/src/main/java/com/model/FasciaOraria.java
@@ -0,0 +1,37 @@
+package com.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fasceOrarie") 
+public class FasciaOraria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
