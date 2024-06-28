ed4fd5dc4d1793d6d52550cc7ef19e669e88a627
Pako9129 <109146197+Pako9129@users.noreply.github.com>
2024-06-28 17:43:47 +0200

    Creazione Repository utente
    Modifiche e creazione Repository

--- ReportTracker/src/main/java/com/repositories/ComuneRepository.java
+++ ReportTracker/src/main/java/com/repositories/ComuneRepository.java
@@ -1,9 +1,10 @@
 package com.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.model.Comune;
-
+@Repository
 public interface ComuneRepository extends JpaRepository<Comune, Long>{
 
 }
--- ReportTracker/src/main/java/com/repositories/UtenteRepository.java
+++ ReportTracker/src/main/java/com/repositories/UtenteRepository.java
@@ -3,8 +3,8 @@ package com.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.model.Segnalazione;
-@Repository 
-public interface SegnalazioneRepository extends JpaRepository <Segnalazione,Long> {
+import com.model.Utente;
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Long>{
 
 }
