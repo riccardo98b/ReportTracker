1c7e52c3d9e32b974d54312b12b38939bca4a29f
nicolozorzi <nicolozorzi96@gmail.com>
2024-06-28 13:00:37 +0200

    tabella comuni
    creata la classe comune

---
+++ ReportTracker/src/main/java/com/model/Comune.java
@@ -0,0 +1,39 @@
+package com.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "comuni")
+public class Comune {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, unique=true)
+	private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	
+	
+}
