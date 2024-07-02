a35f44cf88658f479d7c5f469f2d2a5a24cb2ec3
riccardo98b <riccardobelloni98@gmail.com>
2024-07-02 10:03:39 +0200

    aggiunta altri metodi a fasciaorariacontroller
    aggiunta altri metodi a fasciaorariacontroller

--- ReportTracker/src/main/java/com/controller/FasciaOrariaController.java
+++ ReportTracker/src/main/java/com/controller/FasciaOrariaController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
@@ -42,13 +45,75 @@ public class FasciaOrariaController {
 			}
 		}
 		
-		@PostMapping
+		// Visualizza una FasciaOraria tramite id
+	    @GetMapping("/{id}")
+	    public ResponseEntity<?> findById(@PathVariable Long id) {
+	        try {
+	            FasciaOraria fasciaOraria = service.getFasciaOrariaById(id);
+	            if (fasciaOraria != null) {
+	                return new ResponseEntity<>(fasciaOraria, HttpStatus.OK);
+	            } else {
+	                return new ResponseEntity<>("Fascia oraria non trovata", HttpStatus.NOT_FOUND);
+	            }
+	        } catch (Exception e) {
+	            return new ResponseEntity<>("Errore durante il recupero della fascia oraria", HttpStatus.INTERNAL_SERVER_ERROR);
+	        }
+	    }
+		
+		@PostMapping("/creafascia")
 	    public ResponseEntity<FasciaOraria> createFasciaOraria(@RequestBody FasciaOraria fasciaOraria) {
 	        FasciaOraria nuovaFasciaOraria = service.addFasciaOraria(fasciaOraria);
 	        return ResponseEntity.ok(nuovaFasciaOraria);
 	    }
 		
+		// Cancella una FasciaOraria teamite un id
+	    @DeleteMapping("/cancella/{id}")
+	    public ResponseEntity<?> deleteFasciaOraria(@PathVariable Long id) {
+	        try {
+	            FasciaOraria existingFasciaOraria = service.getFasciaOrariaById(id);
+	            if (existingFasciaOraria != null) {
+	                service.removeFasciaOraria(id);
+	                return new ResponseEntity<>("Fascia oraria eliminata con successo", HttpStatus.OK);
+	            } else {
+	                return new ResponseEntity<>("Fascia oraria non trovata", HttpStatus.NOT_FOUND);
+	            }
+	        } catch (Exception e) {
+	            return new ResponseEntity<>("Errore durante l'eliminazione della fascia oraria", HttpStatus.INTERNAL_SERVER_ERROR);
+	        }
+	    }
+	    
+	 // Aggiorna una FasciaOraria esistente
+	    @PutMapping("/aggiorna/{id}")
+	    public ResponseEntity<?> updateFasciaOraria(@PathVariable Long id, @RequestBody FasciaOraria updatedFasciaOraria) {
+	        try {
+	            FasciaOraria existingFasciaOraria = service.getFasciaOrariaById(id);
+	            if (existingFasciaOraria != null) {
+	                updatedFasciaOraria.setId(id); 
+	                service.updateFasciaOraria(updatedFasciaOraria);
+	                return new ResponseEntity<>(updatedFasciaOraria, HttpStatus.OK);
+	            } else {
+	                return new ResponseEntity<>("Fascia oraria non trovata", HttpStatus.NOT_FOUND);
+	            }
+	        } catch (Exception e) {
+	            return new ResponseEntity<>("Errore durante l'aggiornamento della fascia oraria", HttpStatus.INTERNAL_SERVER_ERROR);
+	        }
+	    
+	        
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	}
+}
+		
 		
 		
 
-	}
+	
