eec1c2b5e0892307d64693d44f3fde4be02ada6a
Samuele Lettieri <samuele2073@gmail.com>
2024-07-04 12:21:31 +0200

    delete comune
    implementata la cancelazione del comune

--- ReportTracker/src/main/java/com/controller/ComuniController.java
+++ ReportTracker/src/main/java/com/controller/ComuniController.java
@@ -1,15 +1,10 @@
 package com.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.model.Comune;
 import com.repositories.ComuneRepository;
 
 
@@ -31,21 +26,6 @@ public class ComuniController {
 	public String create() {
 		return "/comuni/create";
 	}
-	
-/*	@GetMapping("/update/{id}")
-	public String update(@PathVariable Long id, Model model) {
-		Optional<Comune> c = repository.findById(id);
-		
-		if (c.isEmpty()) {
-			return "redirect:comuni/comuniIndex";
-		}
-		
-		
-		model.addAttribute("comune", c.get());
-		
-		
-		return "/comuni/update";
-	}*/
 	
 	@GetMapping("/update/{id}")
 	public String update() {
--- ReportTracker/src/main/resources/static/js/comuni api/index.js
+++ ReportTracker/src/main/resources/static/js/comuni api/index.js
@@ -15,11 +15,30 @@ document.addEventListener("DOMContentLoaded", function() {
             comuniList.innerHTML = '<li>Nessun comune trovato</li>';
         } else {
             data.forEach(comune => {
-                const li = document.createElement('li');
-                li.textContent = comune.nome;
-                comuniList.appendChild(li);
+                comuniList.innerHTML += "<tr> <td>" + comune.id + "</td> <td>" + comune.nome + "</td> <td><a href='http://localhost:8080/comuni/update/" + comune.id + "'>Modifica</a></td> <td><a href='#' class='delete-link' onclick='deleteComune("+comune.id+")'>Elimina</a></td> </tr>";
             });
         }
     })
     .catch(error => console.error('Error fetching comuni:', error));   
 });
+
+function deleteComune(id){
+    console.log(id);
+    fetch(`/comuneAD/delete/${id}`, {
+        method: 'DELETE'
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text();
+    })
+    .then(message => {
+        alert(message);
+
+        // Rimuovi la riga dalla tabella
+        this.closest('tr').remove();
+    })
+    .catch(error => console.error('Error deleting comune:', error));
+
+}
