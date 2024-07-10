e84869382598de8c34a1591c3c6e116ab2916149
Samuele Lettieri <samuele2073@gmail.com>
2024-07-10 12:47:14 +0200

    prova registrati

--- ReportTracker/src/main/java/com/security/SecurityConfig.java
+++ ReportTracker/src/main/java/com/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
             )
             .exceptionHandling(exception -> exception
                 .accessDeniedPage("/errore404")
-            );
+            );   
 
         return http.build();
     }
