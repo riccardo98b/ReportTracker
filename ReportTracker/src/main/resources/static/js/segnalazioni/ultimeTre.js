e9653ab1a889aad2ee3cc94730a31ba0293605d3
riccardo98b <riccardobelloni98@gmail.com>
2024-07-10 17:27:37 +0200

    file js ultime 3 segnalazioni
    file js ultime 3 segnalazioni

---
+++ ReportTracker/src/main/resources/static/js/segnalazioni/ultimeTre.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('/segnalazioni/mie')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        if (response.status === 204) {
+            return [];
+        }
+        return response.json();
+    })  
+    .then(data => {
+        console.log(data);
+        const segnalazioniList = document.getElementById('segnalazioniList');
+        if (data.length === 0) {
+            segnalazioniList.innerHTML = '<li>Nessun segnalazione trovata</li>';
+        } else {
+            for (let i = 3; i >= 0; i--) {
+                let segnalazioni = data[i];
+                segnalazioniList.innerHTML += `<div class="row mt-5">
+                    <div class="col-12 row justify-content-center">
+                        <div class="col-6 row justify-content-center" >
+                            <div class="col-12 ms_box">
+                                <div class="col-12 p-3">
+                                    <h3 class="col-4">${segnalazioni.comune.nome}</h3>
+                                </div>
+                                <div class="col-12 p-3"><img class="ms_immage ms_radius" src="${segnalazioni.foto_o_video}" alt=""></div>
+                                <div class="col-12 p-3 fs-5">${segnalazioni.descrizione}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+            }
+        }
+    })
+    .catch(error => console.error('Error fetching comuni:', error));   
+});
